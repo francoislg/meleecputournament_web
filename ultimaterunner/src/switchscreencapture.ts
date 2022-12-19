60496c226b084fbdeafa2050936986baece066c2
francoislg <francois.lguillemette@gmail.com>
2022-12-19 16:44:14 -0500

    Update for OBS 28+

--- ultimaterunner/src/switchscreencapture.ts
+++ ultimaterunner/src/switchscreencapture.ts
@@ -1,6 +1,7 @@
 // list: ffmpeg -list_devices true -f dshow -i dummy
 // ffmpeg -f dshow -i video="USB3.0 HD Video Capture" -vframes 1 -q:v 2 output.jpg -y
 // when OBS is running: ffmpeg -rtbufsize 100M -f dshow -i video="OBS-Camera" -vframes 1 -q:v 2 output.jpg -y
+// when OBS 28+ is running: ffmpeg -rtbufsize 100M -f dshow -i video="OBS Virtual Camera" -vframes 1 -q:v 2 output.jpg -y
 
 import { promisify } from 'util';
 import { exec } from 'child_process';
@@ -8,7 +9,9 @@ import Jimp from 'jimp';
 import { Image } from './screencapture';
 import { importantLog } from './log';
 
-const DEVICE = 'OBS-Camera';
+// OBS <28 with Virtual Camera plugin
+// const DEVICE = 'OBS-Camera';
+const DEVICE = 'OBS Virtual Camera';
 const FILE = 'pic.jpg';
 
 const run = promisify(exec);
